4a7edef0e1aade69d40ff87c68f17a24d768f2a3
Neelesh Roy <neeleshroy.2013@gmail.com>
2018-05-24 14:20:43 +0530

    added footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import style from './footer.css';
+import { Link } from 'react-router-dom';
+import { CURRENT_YEAR } from '../../constants';
 
 const Footer = () => {
-	return <div>footer</div>;
+	return (
+		<div className={style.footer}>
+			<Link to="/" className={style.logo}>
+				<img alt="nba logo" src="/images/nba_logo.png" />
+			</Link>
+			<div className={style.right}>NBA {CURRENT_YEAR} @All rights reserved.</div>
+		</div>
+	);
 };
 
 export default Footer;
